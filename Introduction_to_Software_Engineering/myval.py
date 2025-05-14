cebd338d85029a7e525ef09e00e8b90086624e9e
Miracle <002nasya@gmail.com>
2025-05-14 23:34:27 +0200

    Add myval.py with value checking and message printing functionality

---
+++ Introduction_to_Software_Engineering/myval.py
@@ -0,0 +1,8 @@
+# This code checks the value of myval and prints a message based on its value.
+myval= 70
+if myval>= 90:
+    print("Excellent")
+elif myval>= 80:
+    print("Good work, keep it up")
+else:
+    print("You need to work harder")
