5c7087085ccd9d785e815f7311b8943dad17c3fa
Miracle <002nasya@gmail.com>
2025-05-15 00:50:34 +0200

    Add while loop implementation with value initialization and break condition

---
+++ Introduction_to_Software_Engineering/while-loop03.py
@@ -0,0 +1,7 @@
+# The code initializes a variable myval with a value of 45 and enters a while loop that continues as long as myval is less than 90.
+myval = 45
+while myval < 90:
+    print(myval)
+    if myval > 80:
+        break
+    myval += 8    
